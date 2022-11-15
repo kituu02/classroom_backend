2d8e44266b56fc3db60e9d50c5ba6f7de23193f4
kituu02 <vvkrishna311@gmail.com>
2022-11-15 21:18:59 +0530

    filesadded

--- src/main/java/com/example/connections.java
+++ src/main/java/com/example/connections.java
@@ -2,10 +2,7 @@ package com.example;
 
 public class connections {
     public static final String driver = "org.postgresql.Driver";
-    //vamsi
-    // public static final String url = "jdbc:postgresql://localhost:5432/postgres";
-    //saketh
-    public static final String url = "jdbc:postgresql://localhost:9090/postgres";
+    public static final String url = "jdbc:postgresql://localhost:5432/classroom";
     public static final String name = "postgres";
     public static final String password = "yourpassword";
 }
